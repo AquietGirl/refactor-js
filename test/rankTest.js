8b98ea34898ccd15e24cde59343e3dc57d58085d
HYS <1683340482@qq.com>
2020-08-28 01:50:27 +0800

    [refactorJs][Alisa] test:rankTest1.Voyage length more than 10 and zone is west-indies. History length is 4

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,4 +1,5 @@
 const rankTest = require('ava');
+import {rating} from '../src/rank'
 
 rankTest('foo', t => {
   t.pass();
@@ -8,3 +9,33 @@ rankTest('bar', async t => {
   const bar = Promise.resolve('bar');
   t.is(await bar, 'bar');
 });
+
+rankTest('rankTest1.Voyage length more than 10 and zone is west-indies. History length is 4', t => {
+  //given
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+
+  //when
+  const result = rating(voyage, history);
+
+  //then
+  t.is("B", result)
+})
