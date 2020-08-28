89f9d2e1d4ed752957ae8b88a24b1137c44ba588
huangal5 <alisa.y.s.huang@oocl.com>
2020-08-28 21:31:07 +0800

    [refactorJs][Alisa] refactor: extract getDeliveryTimeByRush and getDeliveryTimeByNotRush function

--- src/delivery.js
+++ src/delivery.js
@@ -1,25 +1,29 @@
-function deliveryDate(anOrder, isRush) {
-  if (isRush) {
-    let deliveryTime;
-    if (["MA", "CT"].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    } else if (["NY", "NH"].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    } else {
-      deliveryTime = 3;
-    }
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
+function getDeliveryTimeByRush(anOrder) {
+  let deliveryTime;
+  if (["MA", "CT"].includes(anOrder.deliveryState)) {
+    deliveryTime = 1;
+  } else if (["NY", "NH"].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
   } else {
-    let deliveryTime;
-    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    } else if (["ME", "NH"].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    } else {
-      deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+    deliveryTime = 3;
   }
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+function getDeliveryTimeByNotRush(anOrder) {
+  let deliveryTime;
+  if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  } else if (["ME", "NH"].includes(anOrder.deliveryState)) {
+    deliveryTime = 3;
+  } else {
+    deliveryTime = 4;
+  }
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
+function deliveryDate(anOrder, isRush) {
+  return isRush ? getDeliveryTimeByRush(anOrder) : getDeliveryTimeByNotRush(anOrder);
 }
 
 module.exports = {
