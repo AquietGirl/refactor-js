31925f6043e87d5744b18c1917e280617eff0f50
huangal5 <alisa.y.s.huang@oocl.com>
2020-08-28 20:02:47 +0800

    [refactorJs][Alisa] refactor: extract calculateResultByLength function

--- src/rank.js
+++ src/rank.js
@@ -2,21 +2,21 @@ const zones = ["china", "east-indies"];
 
 function voyageRisk(voyage) {
   let result = 1;
-  const voyageLength = voyage.length;
-  if (voyageLength > 4) {
-    result += 2;
-  }
-  if (voyageLength > 8) {
-    result += voyageLength - 8;
-  }
+  result += calculateResultByLength(voyage, 4, 2);
+  result += calculateResultByLength(voyage, 8, voyage.length - 8);
+  
   if (zones.includes(voyage.zone)) {
     result += 4;
   }
   return Math.max(result, 0);
 }
 
-function hasChina(history) {
-  return history.some((v) => zones[0] === v.zone);
+function hasChina(voyage, history) {
+  return voyage.zone === zones[0] && history.some((v) => zones[0] === v.zone);
+}
+
+function calculateResultByLength(obj, length, result) {
+  return obj.length > length ? result : 0;
 }
 
 function captainHistoryRisk(voyage, history) {
@@ -25,7 +25,7 @@ function captainHistoryRisk(voyage, history) {
     result += 4;
   }
   result += history.filter((v) => v.profit < 0).length;
-  if (voyage.zone === zones[0] && hasChina(history)) {
+  if (hasChina(voyage, history)) {
     result -= 2;
   }
   return Math.max(result, 0);
@@ -36,24 +36,14 @@ function voyageProfitFactor(voyage, history) {
   if (zones.includes(voyage.zone)) {
     result += 1;
   }
-  if (voyage.zone === zones[0] && hasChina(history)) {
+  if (hasChina(voyage, history)) {
     result += 3;
-    if (history.length > 10) {
-      result += 1;
-    }
-    if (voyage.length > 12) {
-      result += 1;
-    }
-    if (voyage.length > 18) {
-      result -= 1;
-    }
+    result += calculateResultByLength(history, 10, 1);
+    result += calculateResultByLength(voyage, 12, 1);
+    result += calculateResultByLength(voyage, 18, -1);
   } else {
-    if (history.length > 8) {
-      result += 1;
-    }
-    if (voyage.length > 14) {
-      result -= 1;
-    }
+    result += calculateResultByLength(history, 8, 1);
+    result += calculateResultByLength(voyage, 14, -1);
   }
   return result;
 }
