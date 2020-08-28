f8848385f350820f067c66679abff7509e67cfb3
huangal5 <alisa.y.s.huang@oocl.com>
2020-08-28 17:12:32 +0800

    [refactorJs][Alisa] test: rankTest3.Voyage length is 2 and zone is china. History length is 8

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,131 +1,216 @@
-const rankTest = require('ava');
-import {rating} from '../src/rank'
+const rankTest = require("ava");
+import { rating } from "../src/rank";
 
-rankTest('foo', t => {
+rankTest("foo", (t) => {
   t.pass();
 });
 
-rankTest('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
+rankTest("bar", async (t) => {
+  const bar = Promise.resolve("bar");
+  t.is(await bar, "bar");
 });
 
-rankTest('rankTest1.Voyage length is 10 and zone is west-indies. History length is 4', t => {
-  //given
-  const voyage = {
-    zone: 'west-indies',
-    length: 10,
-  };
-  const history = [
-    {
-      zone: 'east-indies',
-      profit: 5,
-    },{
-      zone: 'west-indies',
-      profit: 15,
-    },{
-      zone: 'china',
-      profit: -2,
-    },
-    {
-      zone: 'west-africa',
-      profit: 7,
-    },
-  ];
+const history = [
+  {
+    zone: "east-indies",
+    profit: 5,
+  },
+  {
+    zone: "west-indies",
+    profit: 15,
+  },
+  {
+    zone: "china",
+    profit: -2,
+  },
+  {
+    zone: "west-africa",
+    profit: 7,
+  },
+];
 
-  //when
-  const result = rating(voyage, history);
+const historyL = [
+  {
+    zone: "east-indies",
+    profit: 5,
+  },
+  {
+    zone: "west-indies",
+    profit: 15,
+  },
+  {
+    zone: "china",
+    profit: -2,
+  },
+  {
+    zone: "west-africa",
+    profit: 7,
+  },
+  {
+    zone: "east-indies",
+    profit: 5,
+  },
+  {
+    zone: "west-indies",
+    profit: 15,
+  },
+  {
+    zone: "china",
+    profit: -2,
+  },
+  {
+    zone: "west-africa",
+    profit: 7,
+  },
+];
 
-  //then
-  t.is("B", result)
-})
+const historyXL = [
+  {
+    zone: "east-indies",
+    profit: 5,
+  },
+  {
+    zone: "west-indies",
+    profit: 15,
+  },
+  {
+    zone: "china",
+    profit: -2,
+  },
+  {
+    zone: "west-africa",
+    profit: 7,
+  },
+  {
+    zone: "east-indies",
+    profit: 5,
+  },
+  {
+    zone: "west-indies",
+    profit: 15,
+  },
+  {
+    zone: "china",
+    profit: -2,
+  },
+  {
+    zone: "west-africa",
+    profit: 7,
+  },
+  {
+    zone: "west-indies",
+    profit: 15,
+  },
+  {
+    zone: "china",
+    profit: -2,
+  },
+  {
+    zone: "west-africa",
+    profit: 7,
+  },
+];
 
-rankTest('rankTest1.Voyage length is 5 and zone is west-indies. History length is 4', t => {
-  //given
-  const voyage = {
-    zone: 'east-indies',
-    length: 5,
-  };
-  const history = [
-    {
-      zone: 'east-indies',
-      profit: 5,
-    },{
-      zone: 'west-indies',
-      profit: 15,
-    },{
-      zone: 'china',
-      profit: -2,
-    },
-    {
-      zone: 'west-africa',
-      profit: 7,
-    },
-  ];
+rankTest(
+  "rankTest0.Voyage length is 10 and zone is west-indies. History length is 4",
+  (t) => {
+    //given
+    const voyage = {
+      zone: "west-indies",
+      length: 10,
+    };
 
-  //when
-  const result = rating(voyage, history);
+    //when
+    const result = rating(voyage, history);
 
-  //then
-  t.is("B", result)
-})
+    //then
+    t.is("B", result);
+  }
+);
 
-rankTest('rankTest2.Voyage length is 5 and zone is east-indies. History length is 4', t => {
-  //given
-  const voyage = {
-    zone: 'east-indies',
-    length: 5,
-  };
-  const history = [
-    {
-      zone: 'east-indies',
-      profit: 5,
-    },{
-      zone: 'west-indies',
-      profit: 15,
-    },{
-      zone: 'china',
-      profit: -2,
-    },
-    {
-      zone: 'west-africa',
-      profit: 7,
-    },
-  ];
+rankTest(
+  "rankTest1.Voyage length is 5 and zone is west-indies. History length is 4",
+  (t) => {
+    //given
+    const voyage = {
+      zone: "east-indies",
+      length: 5,
+    };
+    const history = [
+      {
+        zone: "east-indies",
+        profit: 5,
+      },
+      {
+        zone: "west-indies",
+        profit: 15,
+      },
+      {
+        zone: "china",
+        profit: -2,
+      },
+      {
+        zone: "west-africa",
+        profit: 7,
+      },
+    ];
 
-  //when
-  const result = rating(voyage, history);
+    //when
+    const result = rating(voyage, history);
 
-  //then
-  t.is("B", result)
-})
+    //then
+    t.is("B", result);
+  }
+);
 
-rankTest('rankTest3.Voyage length is 2 and zone is china. History length is 4', t => {
-  //given
-  const voyage = {
-    zone: 'china',
-    length: 2,
-  };
-  const history = [
-    {
-      zone: 'east-indies',
-      profit: 5,
-    },{
-      zone: 'west-indies',
-      profit: 15,
-    },{
-      zone: 'china',
-      profit: -2,
-    },
-    {
-      zone: 'west-africa',
-      profit: 7,
-    },
-  ];
+rankTest(
+  "rankTest2.Voyage length is 5 and zone is east-indies. History length is 4",
+  (t) => {
+    //given
+    const voyage = {
+      zone: "east-indies",
+      length: 5,
+    };
+    const history = [
+      {
+        zone: "east-indies",
+        profit: 5,
+      },
+      {
+        zone: "west-indies",
+        profit: 15,
+      },
+      {
+        zone: "china",
+        profit: -2,
+      },
+      {
+        zone: "west-africa",
+        profit: 7,
+      },
+    ];
 
-  //when
-  const result = rating(voyage, history);
+    //when
+    const result = rating(voyage, history);
 
-  //then
-  t.is("B", result)
-})
+    //then
+    t.is("B", result);
+  }
+);
+
+rankTest(
+  "rankTest3.Voyage length is 2 and zone is china. History length is 8",
+  (t) => {
+    //given
+    const voyage = {
+      zone: "china",
+      length: 2,
+    };
+
+    //when
+    const result = rating(voyage, historyL);
+
+    //then
+    t.is("A", result);
+  }
+);
