a7b713c78f556e66e130ba8a3da4597a1034abf2
HYS <1683340482@qq.com>
2020-08-29 01:40:58 +0800

    [refactorJs][Alisa] refactor:extract rules about deliveryState

--- src/delivery.js
+++ src/delivery.js
@@ -1,31 +1,78 @@
-function getDeliveryTimeByRush(anOrder) {
-  let deliveryTime;
-  if (["MA", "CT"].includes(anOrder.deliveryState)) {
-    deliveryTime = 1;
-  } else if (["NY", "NH"].includes(anOrder.deliveryState)) {
-    deliveryTime = 2;
-  } else {
-    deliveryTime = 3;
+const rulesIsRush = [
+  {
+    match: function (deliveryState) {
+      return ["MA", "CT"].includes(deliveryState);
+    },
+    action: function () {
+      return 1
+    }
+  }, {
+    match: function (deliveryState) {
+      return ["NY", "NH"].includes(deliveryState);
+    },
+    action: function () {
+      return 2
+    }
+  }, {
+    match: function (deliveryState) {
+      return true;
+    },
+    action: function () {
+      return 3
+    }
   }
+];
+const rulesIsNotRush = [
+  {
+    match: function (deliveryState) {
+      return ["MA", "CT", "NY"].includes(deliveryState);
+    },
+    action: function () {
+      return 2;
+    }
+  }, {
+    match: function (deliveryState) {
+      return ["ME", "NH"].includes(deliveryState);
+    },
+    action: function () {
+      return 3;
+    },
+  }, {
+    match: function (deliveryState) {
+      return true;
+    },
+    action: function () {
+      return 4;
+    }
+  }
+];
+
+function getDeliveryTimeByRush(anOrder) {
+    let deliveryTime;
+    for (rule of rulesIsRush) {
+      if (rule.match(anOrder.deliveryState)) {
+        deliveryTime = rule.action();
+        break
+      }
+    }
   return anOrder.placedOn.plusDays(1 + deliveryTime);
 }
 
 function getDeliveryTimeByNotRush(anOrder) {
-  let deliveryTime;
-  if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) {
-    deliveryTime = 2;
-  } else if (["ME", "NH"].includes(anOrder.deliveryState)) {
-    deliveryTime = 3;
-  } else {
-    deliveryTime = 4;
-  }
-  return anOrder.placedOn.plusDays(2 + deliveryTime);
+    let deliveryTime;
+    for (rule of rulesIsNotRush) {
+        if (rule.match(anOrder.deliveryState)) {
+            deliveryTime = rule.action();
+            break;
+        }
+    }
+    return anOrder.placedOn.plusDays(2 + deliveryTime);
 }
 
 function deliveryDate(anOrder, isRush) {
-  return isRush ? getDeliveryTimeByRush(anOrder) : getDeliveryTimeByNotRush(anOrder);
+    return isRush ? getDeliveryTimeByRush(anOrder) : getDeliveryTimeByNotRush(anOrder);
 }
 
 module.exports = {
-  deliveryDate,
+    deliveryDate,
 };
