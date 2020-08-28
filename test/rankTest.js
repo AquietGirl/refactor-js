9617a0b7f365b8a2cd776c94bbd53a4bd15bc078
huangal5 <alisa.y.s.huang@oocl.com>
2020-08-28 12:52:29 +0800

    [refactorJs][Alisa] test: rankTest2.Voyage length is 5 and zone is east-indies. History length is 4

--- test/rankTest.js
+++ test/rankTest.js
@@ -10,11 +10,41 @@ rankTest('bar', async t => {
   t.is(await bar, 'bar');
 });
 
-rankTest('rankTest1.Voyage length more than 10 and zone is west-indies. History length is 4', t => {
+rankTest('rankTest1.Voyage length is 10 and zone is west-indies. History length is 4', t => {
   //given
   const voyage = {
     zone: 'west-indies',
     length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+
+  //when
+  const result = rating(voyage, history);
+
+  //then
+  t.is("B", result)
+})
+
+rankTest('rankTest1.Voyage length is 5 and zone is west-indies. History length is 4', t => {
+  //given
+  const voyage = {
+    zone: 'east-indies',
+    length: 5,
   };
   const history = [
     {
